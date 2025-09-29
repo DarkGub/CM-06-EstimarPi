b8983753bb3edfd5966be259679374dacfd1e82a
Gadiel Ubaldo Corvera Machado <gadielubaldo@gmail.com>
2025-09-29 19:55:54 +0000

    Commit message: Estimar el valor de Pi usando el método de Monte Carlo

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+let puntos = 1000
+let puntosDentro = 0
+let inputPuntos
+let botonEjecutar
+let colorCuadrado
+let colorCirculo
+
 function preload() {
   // put preload code here
 }
@@ -5,10 +12,44 @@ function preload() {
 function setup() {
   // put setup code here
   createCanvas(800,800)
+  noLoop()
+  rectMode(CENTER)
+  colorCuadrado = createColorPicker("blue")
+  colorCirculo = createColorPicker("red")
+  inputPuntos = createInput(puntos)
+  botonEjecutar = createButton("Ejecutar")
+  botonEjecutar.mousePressed(()=>{
+    puntos = int(inputPuntos.value())
+    puntosDentro = 0
+    redraw()
+  })
 }
 
 function draw() {
   // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+  translate(width/2,height/2)
+  let cCirc = colorCirculo.color()
+  let cCuad = colorCuadrado.color()
+  fill(cCuad)
+  rect(0,0,width,height)
+  fill(cCirc)
+  circle(0,0,width)
+  strokeWeight(4)
+  stroke(0)
+  for (let i=0; i<puntos; i++) {
+    let x = random(-width/2,width/2)
+    let y = random(-height/2,height/2)
+    let d = dist(0,0,x,y)
+    point(x,y)
+    if (d < width/2) {
+      puntosDentro++
+    }
+  }
+  let pi = 4 * puntosDentro / puntos
+  fill(255)
+  textsize(32)
+  text("Puntos: " + puntos, -390, 280)
+  text("Puntos dentro del circulo: " + puntosDentro)
+  text("Aproximacion de pi: " + pi)
+  text("Error: " + abs(PI - pi))
 }
